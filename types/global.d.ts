f30fbeca6c73dc93563b37fe6993baad423a862d
一盒十三香 <1186874557@qq.com>
2024-10-31 17:14:17 +0800

    style: :lipstick: 配置文件的修改

--- types/global.d.ts
+++ types/global.d.ts
@@ -3,6 +3,7 @@ declare module "vue" {
     CnButton: (typeof import("cangni"))["CnButton"];
     CnButtonDialog: (typeof import("cangni"))["CnButtonDialog"];
     CnDateRange: (typeof import("cangni"))["CnDateRange"];
+    CnSelect: (typeof import("cangni"))["CnSelect"];
   }
 }
 export {};
